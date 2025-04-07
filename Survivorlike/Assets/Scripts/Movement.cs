13705123a5cc641fa7db13ca4590603cd66c24ae
steeley21 <130697906+steeley21@users.noreply.github.com>
2025-04-07 15:40:07 -0700

    asset location changes and small updates

---
+++ Survivorlike/Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector3 movement;
+    [SerializeField] float speed = 3.0f;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        movement = new Vector3();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement *= speed;
+
+        rb.velocity = movement;
+    }
+}
