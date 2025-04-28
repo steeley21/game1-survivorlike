44d472b4fdfc2f635256b8793393bfd3a4310fa4
csargent3 <csargent3@ewu.edu>
2025-04-27 17:14:58 -0700

    trying something (pls work)

---
+++ Survivorlike/Assets/Scripts/AddPowerup.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPowerup : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float healPoints = 20.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && gameObject.tag == "HealthPowerup")
+        {
+            Debug.Log("Touched health powerup");
+            other.GetComponent<PlayerStats>().HealDamage(healPoints);
+            Destroy(gameObject);
+        } else if(other.CompareTag("Player") && gameObject.tag == "SpeedPowerup")
+        {
+            Debug.Log("Touched speed powerup");
+            Destroy(gameObject);
+        }
+    }
+}
